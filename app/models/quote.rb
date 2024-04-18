ba427311937bc99043b057f0fe03955b35ea297b
veeren08 <veerendragupta52525@gmail.com>
2024-04-18 13:11:44 +0530

    Optimize the broadcasting asynchronous with ActiveJob

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -8,7 +8,11 @@ class Quote < ApplicationRecord
   # The locals default value is equal to { model_name.element.to_sym => self } which, in in the context of our Quote model, is equal to { quote: self }
   # => after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
 
-  after_create_commit -> { broadcast_prepend_to "quotes" }
-  after_update_commit -> { broadcast_replace_to "quotes" }
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+
+
+  # Those three callbacks are equivalent to the following single line
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
