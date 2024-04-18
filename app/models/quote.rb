121261d454947d661bbc367e2d136bc22b9d05a8
veeren08 <veerendragupta52525@gmail.com>
2024-04-18 12:55:42 +0530

    Optimize the broadcast prepend to

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,5 +3,10 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # the target option will be equal to model_name.plural, which is equal to "quotes" so we can remove the target
+  # The partial default value is equal to calling to_partial_path on an instance of the model, which by default in Rails for our Quote model is equal to "quotes/quote".
+  # The locals default value is equal to { model_name.element.to_sym => self } which, in in the context of our Quote model, is equal to { quote: self }
+  # => after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+
+  after_create_commit -> { broadcast_prepend_to "quotes" }
 end
